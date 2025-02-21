bd76c68f890d14976f8ae3800a93301812373ffd
pallempativaishnavi@gmail.com <pallempativaishnavi@gmail.com>
2025-02-21 14:49:33 +0530

    connected

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 7494;
 
-app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+const mongoUrl = process.env.mongodb;
+
+mongoose.connect(mongoUrl)
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to database:', error);
+  });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
